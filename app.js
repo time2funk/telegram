4d4c55e9bc16b1c34e6d39c7e7773d43f4276eee
sadman <m.onevegas@gmail.com>
2018-04-03 08:04:48 +0200

    heroku adds

---
+++ app.js
@@ -0,0 +1,1 @@
+require('./test/heroku.js');
--- test/heroku.js
+++ test/heroku.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 
-const url = 'https://launchlibrary.net/1.3/launch';
 const TOKEN = '574129124:AAEh98_LgjD4VR4u4uvl-USZhLYaakPCoqY';
+const herokuName = 'sad-telegram-bot'
+const url = process.env.APP_URL || `https://${herokuName}.herokuapp.com:443`;
 const options = {
   webHook: {
     // Port to which you should bind is assigned to $PORT variable
@@ -13,11 +14,6 @@ const options = {
     // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
-// Heroku routes from port :443 to $PORT
-// Add URL of your app to env variable or enable Dyno Metadata
-// to get this automatically
-// See: https://devcenter.heroku.com/articles/dyno-metadata
-const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, options);
 
 
@@ -26,7 +22,6 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
-// Just to ping!
 bot.on('message', function onMessage(msg) {
   bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
